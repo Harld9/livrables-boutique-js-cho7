6a4700f69045243437d82e483b9c384a171a75e0
Harold <harold.francois@ynov.com>
2026-03-14 11:41:49 +0100

    feat: création de la structure des pages catalogue, panier et favoris et ajout des liens nav

--- frontend/script/main.js
+++ frontend/script/main.js
@@ -1,17 +1,7 @@
-const urlApi = "http://localhost:8080"
-const bouton = document.getElementById('bouton')
+const boutonburger = document.getElementById('boutonburger');
+const listemenu = document.getElementById('listemenu');
 
-function getSneakers(){
-    fetch(`${urlApi}/sneakers`)
-    .then(response => {
-          return response.json() 
-    })
-    .then(data => {
-        console.log(data)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
 
-bouton.addEventListener('click', getSneakers)
+boutonburger.addEventListener('click', () => {
+  listemenu.classList.toggle('active'); 
+});
