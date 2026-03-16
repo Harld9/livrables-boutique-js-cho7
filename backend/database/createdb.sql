33aa5871aea397bffde5ad9b375e1f9e224a76b7
Harold <harold.francois@ynov.com>
2026-03-16 20:23:28 +0100

    refactor: passage des requêtes sql de microsoft access à mysql

--- backend/database/createdb.sql
+++ backend/database/createdb.sql
@@ -1,34 +1,34 @@
 CREATE TABLE Client(
-   IdClient COUNTER,
-   Nom VARCHAR(50),
-   Prénom VARCHAR(50),
-   Adresse VARCHAR(50),
-   Mail VARCHAR(50),
-   NumeroTel INT,
+   IdClient INT AUTO_INCREMENT,
+   Nom VARCHAR(50) NOT NULL,
+   Prenom VARCHAR(50) NOT NULL,
+   Adresse VARCHAR(255),
+   Mail VARCHAR(100) NOT NULL,
+   NumeroTel VARCHAR(20),
    PRIMARY KEY(IdClient)
 );
 
 CREATE TABLE Categorie(
-   IdCategorie COUNTER,
-   NomCategorie VARCHAR(50),
+   IdCategorie INT AUTO_INCREMENT,
+   NomCategorie VARCHAR(50) NOT NULL,
    PRIMARY KEY(IdCategorie)
 );
 
 CREATE TABLE Commande(
-   IdCommande COUNTER,
-   Date_ VARCHAR(50),
+   IdCommande INT AUTO_INCREMENT,
+   DateCommande DATETIME NOT NULL,
    IdClient INT NOT NULL,
    PRIMARY KEY(IdCommande),
    FOREIGN KEY(IdClient) REFERENCES Client(IdClient)
 );
 
 CREATE TABLE Produit(
-   IdProduit COUNTER,
-   NomProduit VARCHAR(50),
-   Prix CURRENCY,
+   IdProduit INT AUTO_INCREMENT,
+   NomProduit VARCHAR(100) NOT NULL,
+   Prix DECIMAL(10,2) NOT NULL,
    Taille INT,
    Genre VARCHAR(50),
-   IdCategorie INT,
+   IdCategorie INT NOT NULL,
    PRIMARY KEY(IdProduit),
    FOREIGN KEY(IdCategorie) REFERENCES Categorie(IdCategorie)
 );
@@ -36,7 +36,7 @@ CREATE TABLE Produit(
 CREATE TABLE Contient(
    IdProduit INT,
    IdCommande INT,
-   Quantite INT,
+   Quantite INT NOT NULL,
    PRIMARY KEY(IdProduit, IdCommande),
    FOREIGN KEY(IdProduit) REFERENCES Produit(IdProduit),
    FOREIGN KEY(IdCommande) REFERENCES Commande(IdCommande)
