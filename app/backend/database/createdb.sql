e1d1687eb86600648c3a67aa663ff73e8c3a21d3
Harold <harold.francois@ynov.com>
2026-03-20 11:19:31 +0100

    refactor: amélioration des requêtes pour créer la base de données

--- app/backend/database/createdb.sql
+++ app/backend/database/createdb.sql
@@ -1,39 +1,44 @@
-CREATE TABLE Client(
-   IdClient COUNTER,
+CREATE TABLE Client (
+   IdClient INT AUTO_INCREMENT,
    Nom VARCHAR(50),
-   Prénom VARCHAR(50),
-   Adresse VARCHAR(50),
-   Mail VARCHAR(50),
-   NumeroTel INT,
+   Prenom VARCHAR(50), 
+   Adresse VARCHAR(255), 
+   Mail VARCHAR(100),
+   NumeroTel VARCHAR(20), 
    PRIMARY KEY(IdClient)
 );
 
-CREATE TABLE Categorie(
-   IdCategorie COUNTER,
+CREATE TABLE Categorie (
+   IdCategorie INT AUTO_INCREMENT,
    NomCategorie VARCHAR(50),
    PRIMARY KEY(IdCategorie)
 );
 
-CREATE TABLE Commande(
-   IdCommande COUNTER,
-   Date_ VARCHAR(50),
+CREATE TABLE Commande (
+   IdCommande INT AUTO_INCREMENT,
+   DateCommande DATETIME, 
    IdClient INT NOT NULL,
    PRIMARY KEY(IdCommande),
    FOREIGN KEY(IdClient) REFERENCES Client(IdClient)
 );
 
-CREATE TABLE Produit(
-   IdProduit COUNTER,
-   NomProduit VARCHAR(50),
-   Prix CURRENCY,
-   Taille INT,
+CREATE TABLE Produit (
+   IdProduit INT AUTO_INCREMENT,
+   NomProduit VARCHAR(100),
+   Reduction DECIMAL(5,2), 
+   Type VARCHAR(50), 
+   Description TEXT, 
+   Prix DECIMAL(10,2), 
+   Taille VARCHAR(20), 
    Genre VARCHAR(50),
    IdCategorie INT,
+   Stock INT,
+   Image VARCHAR(100)
    PRIMARY KEY(IdProduit),
    FOREIGN KEY(IdCategorie) REFERENCES Categorie(IdCategorie)
 );
 
-CREATE TABLE Contient(
+CREATE TABLE Contient (
    IdProduit INT,
    IdCommande INT,
    Quantite INT,
